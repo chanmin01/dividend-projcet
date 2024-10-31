d72c0c1e02816efc435d98806bc9f335e8662e95
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-10-31 17:15:11 +0900

    feat: JWT에서 인증정보를 추출하는 getAuthentification 메서드 추가

--- src/main/java/com/dividend/security/TokenProvider.java
+++ src/main/java/com/dividend/security/TokenProvider.java
@@ -1,12 +1,16 @@
 package com.dividend.security;
 
 
+import com.dividend.service.MemberService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -19,10 +23,13 @@ public class TokenProvider {
 
     private static final String KEY_ROLES = "roles";
     private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60;
+
+    private final MemberService memberService;
+
     @Value("{spring.jwt.secret}")
     private String secretKey;
 
-    private String generateToken(String username, List<String> roles) {
+    public String generateToken(String username, List<String> roles) {
         Claims claims = Jwts.claims().setSubject(username);
         claims.put(KEY_ROLES, roles);
 
@@ -36,6 +43,17 @@ public class TokenProvider {
                 .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
                 .compact();
     }
+
+    /**
+     * jwt로부터 인증정보를 가져오는 메서드
+     * @param jwt
+     */
+    public Authentication getAuthentication(String jwt) {
+        UserDetails userDetails = this.memberService.loadUserByUsername(this.getUsername(jwt));
+
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
 
     public String getUsername(String token) {
         return this.parseClaims(token).getSubject();
