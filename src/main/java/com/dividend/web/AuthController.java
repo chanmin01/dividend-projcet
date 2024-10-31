6f3eee8ed645df14f7f9044e17ef7e0f40f86e9f
chanmin01 <68585373+chanmin01@users.noreply.github.com>
2024-10-31 16:18:06 +0900

    feat: 회원가입 및 로그인 기능 구현

--- src/main/java/com/dividend/web/AuthController.java
+++ src/main/java/com/dividend/web/AuthController.java
@@ -1,5 +1,37 @@
 package com.dividend.web;
 
 
+import com.dividend.model.Auth;
+import com.dividend.model.MemberEntity;
+import com.dividend.security.TokenProvider;
+import com.dividend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final MemberService memberService;
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+        MemberEntity result = this.memberService.register(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> signIn(@RequestBody Auth.SignIn request) {
+        MemberEntity member = this.memberService.authenticate(request);
+        String token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+        return ResponseEntity.ok(token);
+    }
 }
